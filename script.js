8c7b18af01d83f4e42295eac22bf9b37d3c1821b
Guilherme09396 <guilherme09396@gmail.com>
2024-08-07 17:46:19 -0300

    Novas alterações na parte do cardápio para desktop e mobile

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let show = true;
 const menuContent = document.querySelector('.content');
 const menuToggle = menuContent.querySelector('.menu-toggle');
+const slideShow = document.querySelector(".cardapio");
+const linkCardapio = document.querySelector("#link-cardapio")
 
 
 menuToggle.addEventListener('click', () => {
@@ -10,3 +12,8 @@ menuToggle.addEventListener('click', () => {
     menuContent.classList.toggle('on', show);
     show = !show;
 })
+
+if(screen.width > 480) {
+    slideShow.setAttribute("hidden", "true");
+    linkCardapio.setAttribute("href", "https://google.com/search?q=hot+dog+lanches")
+}
